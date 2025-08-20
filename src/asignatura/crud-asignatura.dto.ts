c21077af78a2a6df7590174d74ea6fd2dfe908e7
Alejandro Toro <alejandropinedo@uninorte.edu.co>
2025-08-19 23:23:12 -0500

    Asignatura Dto modified

--- src/asignatura/crud-asignatura.dto.ts
+++ src/asignatura/crud-asignatura.dto.ts
@@ -2,10 +2,12 @@ export class CreateAsignaturaDto {
     nombre: string;
     descripcion: string;
     creditos: number;
+    departamento: string;
 }
 
 export class UpdateAsignaturaDto {
     nombre?: string;
     descripcion?: string;
     creditos?: number;
+    departamento?: string;
 }
