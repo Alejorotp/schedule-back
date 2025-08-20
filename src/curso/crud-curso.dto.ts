5611cd58f6b756ab414e9ed62289dff402c4ebf6
Alejandro Toro <alejandropinedo@uninorte.edu.co>
2025-08-19 23:09:46 -0500

    Added the column periodo to horario and nrc is not unique anymore. Prisma and Dto updated.

--- src/curso/crud-curso.dto.ts
+++ src/curso/crud-curso.dto.ts
@@ -1,11 +1,13 @@
 export class CreateCursoDto {
   nrc: string;
+  periodo: string;
   asignatura_id: number;
   profesor_id?: number;
 }
 
 export class UpdateCursoDto {
   nrc?: string;
+  periodo?: string;
   asignatura_id?: number;
   profesor_id?: number;
 }
